23e3f7e863499221c46a2458e15b104fa65ace8f
TrongNguyen03 <trongnguyenj3@gmail.com>
2025-04-02 15:53:50 +0700

    thêm file

--- BaiTap.java
+++ BaiTap.java
@@ -1,96 +1,96 @@
 import java.text.DecimalFormat;
 import java.util.*;
 public class BaiTap {
-//Bài 1 
-//    static class SanPham{
-//         private String ten;
-//         private int gia;
-//         private int GiamGia;
+//Bài 1
+    static class SanPhamv1{
+         private String ten;
+         private int gia;
+         private int GiamGia;
 
-//         public SanPham(String ten, int gia, int GiamGia){
-//             this.ten=ten;
-//             this.gia= gia;
-//             this.GiamGia= GiamGia;
-//         }
+         public SanPhamv1(String ten, int gia, int GiamGia){
+             this.ten=ten;
+             this.gia= gia;
+             this.GiamGia= GiamGia;
+         }
 
-//         //phương thức tính thuế
-//         public double tinhthuenhapkhau(){
-//             return gia*0.1;
-//         }
-        
-//         //Xuất thông tin
-//         public void xuatthongtin(){
-//             DecimalFormat df = new DecimalFormat("#,###");
-//             System.out.println("Ten san pham: "+ ten);
-//             System.out.println("Gia san pham: "+df.format(gia));
-//             System.out.println("Giam gia: "+ df.format(GiamGia));
-//             System.out.println("Thue nhap khau: "+ df.format(tinhthuenhapkhau()));
-//             System.out.println("Don Gia: "+ df.format((gia+tinhthuenhapkhau()-GiamGia)));
+         //phương thức tính thuế
+         public double tinhthuenhapkhau(){
+             return gia*0.1;
+         }
 
-            
-//         }
-//         public static void main(String[] args) {
-//             SanPham sp= new SanPham("Tu lanh", 10000000, 200000);
-//             sp.xuatthongtin();
-//         }
+         //Xuất thông tin
+         public void xuatthongtin(){
+             DecimalFormat df = new DecimalFormat("#,###");
+             System.out.println("Ten san pham: "+ ten);
+             System.out.println("Gia san pham: "+df.format(gia));
+             System.out.println("Giam gia: "+ df.format(GiamGia));
+             System.out.println("Thue nhap khau: "+ df.format(tinhthuenhapkhau()));
+             System.out.println("Don Gia: "+ df.format((gia+tinhthuenhapkhau()-GiamGia)));
 
-//     }
 
-// Bài 2
-// static class SanPham{
-//     private String ten;
-//     private int gia;
-//     private int GiamGia;
+         }
+         public static void main(String[] args) {
+             SanPhamv1 sp= new SanPhamv1("Tu lanh", 10000000, 200000);
+             sp.xuatthongtin();
+         }
 
-//     public SanPham(String ten, int gia, int GiamGia){
-//         this.ten=ten;
-//         this.gia= gia;
-//         this.GiamGia= GiamGia;
-//     }
+     }
 
-//     //phương thức tính thuế
-//     public double tinhthuenhapkhau(){
-//         return gia*0.1;
-//     }
-    
-//     //Xuất thông tin
-//     public void xuatthongtin(){
-//         DecimalFormat df = new DecimalFormat("#,###");
-//         System.out.println("HANG HOA ");
-//         System.out.println("Ten san pham: "+ ten);
-//         System.out.println("Gia san pham: "+df.format(gia));
-//         System.out.println("Giam gia: "+ df.format(GiamGia));
-//         System.out.println("Thue nhap khau: "+ df.format(tinhthuenhapkhau()));
-//         System.out.println("Don Gia: "+ df.format((gia+tinhthuenhapkhau()-GiamGia)));
+// // Bài 2
+ static class SanPhamv2{
+     private String ten;
+     private int gia;
+     private int GiamGia;
 
-        
-//     }
-//     public static void main(String[] args) {
-//         Scanner scanner=new Scanner(System.in);
-//         ArrayList<SanPham> danhsach =new ArrayList<>();
-//         System.out.println("Nhap so luong san pham:");
-//         int soluong=scanner.nextInt();
-//         scanner.nextLine();
+     public SanPhamv2(String ten, int gia, int GiamGia){
+         this.ten=ten;
+         this.gia= gia;
+         this.GiamGia= GiamGia;
+     }
 
-//         for(int i=0;i< soluong;i++){
-//             System.out.println("Nhap thong tin san pham thu "+(i+1)+":");
-//             System.out.println("Ten san pham: ");
-//             String ten=scanner.nextLine();
-//             System.out.println("Nhap gia san pham: ");
-//             int gia = scanner.nextInt();
-//             System.out.println("Giam gia: ");
-//             int GiamGia=scanner.nextInt();
-//             scanner.nextLine();
-//             danhsach.add(new SanPham(ten, gia, GiamGia));
-//         }
-//         System.out.println("=========Danh Sach Hang Hoa=========");
-//         for (SanPham sp: danhsach){
-//             sp.xuatthongtin();
-//         }
-//         scanner.close();
-//     }
+     //phương thức tính thuế
+     public double tinhthuenhapkhau(){
+         return gia*0.1;
+     }
 
-// }
+     //Xuất thông tin
+     public void xuatthongtin(){
+         DecimalFormat df = new DecimalFormat("#,###");
+         System.out.println("HANG HOA ");
+         System.out.println("Ten san pham: "+ ten);
+         System.out.println("Gia san pham: "+df.format(gia));
+         System.out.println("Giam gia: "+ df.format(GiamGia));
+         System.out.println("Thue nhap khau: "+ df.format(tinhthuenhapkhau()));
+         System.out.println("Don Gia: "+ df.format((gia+tinhthuenhapkhau()-GiamGia)));
+
+
+     }
+     public static void main(String[] args) {
+         Scanner scanner=new Scanner(System.in);
+         ArrayList<SanPhamv2> danhsach =new ArrayList<>();
+         System.out.println("Nhap so luong san pham:");
+         int soluong=scanner.nextInt();
+         scanner.nextLine();
+
+         for(int i=0;i< soluong;i++){
+             System.out.println("Nhap thong tin san pham thu "+(i+1)+":");
+             System.out.println("Ten san pham: ");
+             String ten=scanner.nextLine();
+             System.out.println("Nhap gia san pham: ");
+             int gia = scanner.nextInt();
+             System.out.println("Giam gia: ");
+             int GiamGia=scanner.nextInt();
+             scanner.nextLine();
+             danhsach.add(new SanPhamv2(ten, gia, GiamGia));
+         }
+         System.out.println("=========Danh Sach Hang Hoa=========");
+         for (SanPhamv2 sp: danhsach){
+             sp.xuatthongtin();
+         }
+         scanner.close();
+     }
+
+ }
 
 // Bài 3
 static class SanPham{
@@ -102,7 +102,7 @@ static class SanPham{
         this.gia= gia;
     }
 
-    
+
     //Xuất thông tin
     public void xuatthongtin(){
         DecimalFormat df = new DecimalFormat("#,###");
@@ -159,7 +159,7 @@ static class SanPham{
              System.out.println("Tong trung binh gia san pham: "+ df.format(giatb(danhsach)));
          }
     }
-    
+
     //hàm xóa sp
     public static void timxoasp(List<SanPham> danhsach, String tencanxoa){
         boolean daxoa= danhsach.removeIf(sp -> sp.getten().equals(tencanxoa));
@@ -170,7 +170,7 @@ static class SanPham{
             System.out.println("San pham khong ton tai: "+ tencanxoa);
         }
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         List<SanPham> danhsach=new ArrayList<>();
@@ -191,6 +191,5 @@ static class SanPham{
         }
         scanner.close();
     }
-
 }
 }
