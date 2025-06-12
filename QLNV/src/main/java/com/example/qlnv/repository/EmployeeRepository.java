165f86f7a569146751abd6d08a907e58fa213cb7
TrongNguyen <109862132+TrongNguyen03@users.noreply.github.com>
2025-06-12 10:46:33 +0700

    Delete QLNV directory

--- QLNV/src/main/java/com/example/qlnv/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.qlnv.repository;
-
-
-import com.example.qlnv.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
